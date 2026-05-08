f863f1109b46d49c0ef8d77deb178a9cd4b020f0
Torian10 <tmalcolm@udel.edu>
2026-05-08 07:37:08 -0400

    update

--- project-3-character-creator/control.gd
+++ project-3-character-creator/control.gd
@@ -1,8 +1,10 @@
 extends Control
 
-@onready var eyes = $CompositeSprites/Eyes
-@onready var hair = $CompositeSprites/Hair
-@onready var clothes = $CompositeSprites/Clothes
+@onready var eyes = $"../CompositeSprites/Eyes".get_children()
+@onready var hair = $"../CompositeSprites/Hair".get_children()
+@onready var clothes = $"../CompositeSprites/Clothes".get_children()
+
+
 
 var eye_options = [
 	preload("res://creator creator folder/Character eyes #1.png"), 
@@ -32,77 +34,109 @@ var eye_index = 0
 var hair_index = 0
 var clothes_index = 0
 
-#EYES
-func eye_left():
-	eye_index -= 1
-	if eye_index < 0:
-		eye_index = eye_options.size() - 1
-	eyes.texture = eye_options[eye_index]
 
-func eye_right():
-	eye_index += 1
-	if eye_index >= eye_options.size():
-		eye_index = 0
-	eyes.texture = eye_options[eye_index]
-	
-#HAIR
-func hair_left():
-	hair_index -= 1
-	if hair_index < 0:
-		hair_index = hair_options.size() - 1
-	hair.texture = hair_options[hair_index]
-
-func hair_right():
-	hair_index += 1
-	if hair_index >= hair_options.size():
-		hair_index = 0
-	hair.texture = hair_options[hair_index]
-
-
-#CLOTHES
-func clothes_left():
-	clothes_index -= 1
-	if clothes_index < 0:
-		clothes_index = clothes_options.size() - 1
-	clothes.texture = clothes_options[clothes_index]
-
-func clothes_right():
-	clothes_index += 1
-	if clothes_index >= clothes_options.size():
-		clothes_index = 0
-	clothes.texture = clothes_options[clothes_index]
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	eyes.texture = eye_options[0]
-	hair.texture = hair_options[0]
-	clothes.texture = clothes_options[0]
+	show_only(eyes, eye_index)
+	show_only(hair, hair_index)
+	show_only(clothes, clothes_index)
 	
-	eyes.texture = eye_options[0]
+	for i in range(eyes.size()):
+		eyes[i].visible = false
+
+	eyes[0].visible = true
+
+func show_only(group, index):
+	for item in group:
+		item.visible = false
+	if group.size() > 0:
+		group[index].visible = true
 
 func _on_left_arrow_pressed():
 	eye_index -= 1
 	if eye_index < 0:
-		eye_index = eye_options.size() - 1
-	eyes.texture = eye_options[eye_index]
+		eye_index = eyes.size() - 1
+	show_only(eyes, eye_index)
 
 func _on_right_arrow_pressed():
 	eye_index += 1
-	if eye_index >= eye_options.size():
+	if eye_index >= eyes.size():
 		eye_index = 0
-	eyes.texture = eye_options[eye_index]
+	show_only(eyes, eye_index)
 
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
 
 
 func _on_left_arrow_eye_pressed() -> void:
+	eyes[eye_index].visible = false
+
+	eye_index -= 1
+	if eye_index < 0:
+		eye_index = eyes.size() - 1
+
+	eyes[eye_index].visible = true
+	
 	pass # Replace with function body.
 
 
 func _on_right_arrow_eyes_pressed() -> void:
+	eyes[eye_index].visible = false
+
+	eye_index += 1
+	if eye_index >= eyes.size():
+		eye_index = 0
+
+	eyes[eye_index].visible = true
+	pass # Replace with function body.
+
+
+func _on_left_clothes_button_pressed() -> void:
+	clothes[clothes_index].visible = false
+
+	clothes_index -= 1
+	if clothes_index < 0:
+		clothes_index = clothes.size() - 1
+
+	clothes[clothes_index].visible = true
+	pass # Replace with function body.
+
+
+func _on_right_clothes_button_pressed() -> void:
+	clothes[clothes_index].visible = false
+
+	clothes_index += 1
+	if clothes_index >= clothes.size():
+		clothes_index = 0
+
+	clothes[clothes_index].visible = true
+	pass # Replace with function body.
+
+
+func _on_right_hair_button_pressed() -> void:
+	hair[hair_index].visible = false
+
+	hair_index += 1
+	if hair_index >= hair.size():
+		hair_index = 0
+
+	hair[hair_index].visible = true
+
+	pass # Replace with function body.
+
+
+func _on_left_hair_button_pressed() -> void:
+	hair[hair_index].visible = false
+
+	hair_index -= 1
+	if hair_index < 0:
+		hair_index = hair.size() - 1
+
+	hair[hair_index].visible = true
 	pass # Replace with function body.
